b50c6909e8aacc596fed6cae79e07adb0527f75a
sajjad <sajjad.kazazi@yahoo.com>
2021-01-21 13:54:23 +0330

    git init

---
+++ config/gateway.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+
+    //-------------------------------
+    // Timezone for insert dates in database
+    // If you want Gateway not set timezone, just leave it empty
+    //--------------------------------
+    'timezone' => 'Asia/Tehran',
+
+
+
+    //--------------------------------
+    // Sadad gateway
+    //--------------------------------
+    'sadad' => [
+        'merchant'      => '',
+        'transactionKey'=> '',
+        'terminalId'    => 000000000,
+        'callback-url'  => '/'
+    ],
+
+
+    //--------------------------------
+    // Asan Pardakht gateway
+    //--------------------------------
+    'asanpardakht' => [
+        'merchantId'     => '',
+        'merchantConfigId'     => '',
+        'username' => '',
+        'password' => '',
+        'key' => '',
+        'iv' => '',
+        'callback-url'   => '/',
+    ],
+
+    //--------------------------------
+    // Paypal gateway
+    //--------------------------------
+
+    //-------------------------------
+    // Tables names
+    //--------------------------------
+    'table'    => 'gateway_transactions',
+];
